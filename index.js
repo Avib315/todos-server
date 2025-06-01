1c079366f9e5013da3d03d1a4d115f99fb8ec1fd
user <user@DESKTOP-8867Q6H>
2025-06-01 09:51:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const db = require("./DL/db.js")
+const express = require("express")
+CORS = require("cors"),
+    PORT = 3001
+const app = express()
+db.getConnection().then(connection => {
+    console.log('Database connected successfully');
+    connection.release();
+  })
+  .catch(err => {
+    console.error('Database connection failed:', err);
+  });
+app.use(CORS());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`\x1b[42m [index.js] server is runing in port ${PORT} \x1b[0m`);
+})
