a95c38d62eec1a045b0e36fe84fa9d25b6029a60
coralsh286 <coralsh286@gmail.com>
2025-06-01 10:29:44 +0300

    add routes for todos and comments; set up users and posts routes

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ db.getConnection().then(connection => {
   });
 app.use(CORS());
 app.use(express.json());
+app.use("/users", require("./routes/users.router.js"));
+app.use("/posts", require("./routes/posts.router.js"));
+app.use("/todos", require("./routes/todos.router.js"));
+app.use("/comments", require("./routes/comments.router.js"));
 
 app.listen(PORT, () => {
     console.log(`\x1b[42m [index.js] server is runing in port ${PORT} \x1b[0m`);
